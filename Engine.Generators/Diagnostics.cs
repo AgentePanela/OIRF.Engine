d1c87f449d96941c3cf55313f7f3119b35ae7252
AgentePanela <AgentePanela@gmail.com>
2026-09-10 13:58:37 -0300

    fuck ts shit

--- Engine.Generators/Diagnostics.cs
+++ Engine.Generators/Diagnostics.cs
@@ -3,8 +3,4 @@ namespace Engine.Generators;
 public static class Diagnostics
 {
     public const string StyleConstructorID = "EA001";
-    public const string NetFieldUnsupportedTypeID = "EA002";
-    public const string MessageNotPartialID = "EA003";
-    public const string NoMatchingNetSerializableCtorID = "EA004";
-    public const string CircularNetSerializableID = "EA005";
 }
